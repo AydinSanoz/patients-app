527355aed083cbf2b8b2bcbeeebf6c5c20e58253
Aydin Sanoz <aydinsanoz2@gmail.com>
2023-03-05 22:05:41 +0100

    dependencies installed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,28 +3,23 @@ import "../styles/App.scss";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import connectDB from "@/database/conn";
-import { useEffect } from "react";
+
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import Layout from "@/components/Layout";
-import ThemeProvider from "react-bootstrap/ThemeProvider";
+
+import { QueryClient, QueryClientProvider } from "react-query";
 
 export default function App({ Component, pageProps }) {
-  useEffect(() => {
-    connectDB();
-  }, []);
+
+  const queryClient = new QueryClient();
 
   return (
     <>
-      <ThemeProvider
-        breakpoints={["xxxl", "xxl", "xl", "lg", "md", "sm", "xs", "xxs"]}
-        minBreakpoint="xxs"
-      >
+      <QueryClientProvider client={queryClient}>
         <Header />
         <Component {...pageProps} />
         <Footer />
-      </ThemeProvider>
+      </QueryClientProvider>
     </>
   );
 }
