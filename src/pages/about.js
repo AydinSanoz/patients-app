d6118d1e4e9b308c21d3c89970e7107e3336b2d0
Aydin Sanoz <aydinsanoz2@gmail.com>
2023-03-05 22:05:57 +0100

    about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,6 +2,31 @@ export default function About(props) {
   return (
     <div>
       <h1>About Page</h1>
+      <h2>
+        Non magna sunt ad adipisicing esse veniam voluptate sit est tempor enim
+        ex ad.
+      </h2>
+      <p>
+        Officia amet proident ex mollit pariatur deserunt dolore. Culpa
+        consequat magna minim reprehenderit et veniam reprehenderit. Eiusmod
+        incididunt ad tempor labore. Ut enim laborum velit adipisicing. Occaecat
+        cupidatat sit laborum velit occaecat aute consequat qui ipsum eiusmod
+        commodo exercitation veniam. Aliquip anim dolore sunt voluptate nisi
+        nulla occaecat excepteur consequat exercitation ad irure. In est veniam
+        reprehenderit quis ea aliquip ad cupidatat do fugiat tempor et. Culpa
+        nulla sit eu nisi non veniam aliqua veniam aute excepteur. Ex incididunt
+        cillum ipsum esse. Dolor labore laborum laboris ea sit officia excepteur
+        excepteur quis sint laborum ullamco officia. Ut sit aute pariatur labore
+        ipsum. Occaecat Lorem nostrud mollit commodo consequat laboris elit
+        adipisicing Lorem cillum non nisi. Officia ipsum voluptate pariatur
+        voluptate dolore id magna veniam minim id est dolore.
+      </p>
+      <p>
+        Commodo pariatur ullamco excepteur excepteur ad duis. Amet proident enim
+        magna enim exercitation nostrud ea elit veniam nostrud aliquip qui est
+        nisi. Voluptate Lorem non occaecat ea sit eiusmod ex Lorem magna laborum
+        id anim.
+      </p>
     </div>
   );
 }
